faa3e42d2c8a2753969c2e6e7c3e4dd33ed3e79b
voidxiao <1019308293@qq.com>
2019-09-15 20:41:19 +0800

    继哈希表散列函数和链式存储  添加取哈希表数据

---
+++ 数据结构/取哈希表数据.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct link
+{
+	int data;
+	struct link* next;
+}Link;
+
+//散列函数
+void add(int value, Link** hash);
+
+//取哈希表数据
+Link* getHash(Link** hash, int data);
+
+int main(void)
+{
+	int temp[] = { 11, 32, 22, 21, 31, 50, 53, 23, 15, 25, 52, 111, 122, 123 };
+	int num = sizeof(temp) / 4;
+	Link** hash = (Link**)malloc(sizeof(Link**) * 10);
+	for (int i = 0; i < 10; i++)
+	{
+		hash[i] = NULL;
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		add(temp[i], hash);
+	}
+
+	//取数据
+	Link* data = getHash(hash, 123);
+	printf("%p:%d\n", data, data->data);
+
+
+
+	system("pause");
+	return 0;
+}
+
+//散列函数
+void add(int value, Link** hash)
+{
+	if (hash[value % 10])
+	{
+		Link* Ptemp = hash[value % 10];
+		Link* temp = (Link*)malloc(sizeof(Link));
+		temp->data = value;
+		temp->next = NULL;
+		while (Ptemp)
+		{
+			if (Ptemp->next)
+			{
+				Ptemp = Ptemp->next;
+			}
+			else
+			{
+				Ptemp->next = temp;
+				break;
+			}
+		}
+	}
+	else
+	{
+		Link* temp = (Link*)malloc(sizeof(Link));
+		temp->data = value;
+		temp->next = NULL;
+		hash[value % 10] = temp;
+	}
+}
+
+//取哈希表数据
+Link* getHash(Link** hash, int data)
+{
+	int mod = data % 10;
+	Link* temp = hash[mod];
+	if (hash[mod]->data == data)
+	{
+		return hash[mod];
+	}
+	else
+	{
+		while (temp->next)
+		{
+			if (temp->next->data == data)
+			{
+				return temp->next;
+			}
+			else
+			{
+				temp = temp->next;
+			}
+		}
+		return NULL;
+	}
+}
